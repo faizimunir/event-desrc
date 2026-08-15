0ab2b98dfa30e988ecb6777d53ba9493a61dc8fe
wamyrullah <wamyrullah@gmail.com>
2026-08-15 16:13:47 +0700

    update tampilan

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -1,5 +1,13 @@
-<x-layouts::app :title="__('Edit Event')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <livewire:events.event-form :event="$event" />
+<x-layouts::app :title="__('Edit Event')" :unified-header="true">
+    <div class="flex h-full w-full flex-1 flex-col">
+        <x-admin-detail-hero
+            :heading="__('Edit Event')"
+            :subheading="__('Events')"
+            :back-href="route('events.show', $event)"
+        />
+
+        <div class="users-hero-content flex flex-1 flex-col gap-4 pt-4 pb-6">
+            <livewire:events.event-form :event="$event" />
+        </div>
     </div>
 </x-layouts::app>
